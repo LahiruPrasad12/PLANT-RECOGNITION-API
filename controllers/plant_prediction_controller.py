8ab676bcae7bb876ae43c68fd942ece3dcfdb6f1
LahiruPrasad12 <lahirupr471@gmail.com>
2023-09-01 13:28:04 +0530

    completed save predicted data

--- controllers/plant_prediction_controller.py
+++ controllers/plant_prediction_controller.py
@@ -2,8 +2,11 @@ from flask import Blueprint, jsonify, request
 import numpy as np
 import tensorflow as tf
 import pipe.plant_prediction_pipeline as prediction
+from application import db
 
 plant_prediction_controller = Blueprint('plant_prediction', __name__)
+
+predicted_collection = db["predicted_lists"]
 
 @plant_prediction_controller.route("/plant", methods = ['POST'])
 def plantPrediction():
@@ -40,3 +43,16 @@ def plantPrediction():
             "result_index": test_set.class_names[result_index[0][0]],
         }
         return jsonify(response_data)
+
+@plant_prediction_controller.route("/save", methods=['POST'])
+def save_prediction():
+    data = request.json
+    user_id = data['user_id']
+    url = data['image_url']
+    predicted_name = data['predicted_name']
+    # password = data['password']
+
+    new_item = {"first_name": user_id, "last_name":url,"email": predicted_name}
+    predicted_collection.insert_one(new_item)
+    new_item["_id"] = str(new_item["_id"])
+    return jsonify(new_item), 201
